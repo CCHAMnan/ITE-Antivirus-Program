782c5f29db8359c895966fa56a9031e46b53625c
SGGotcher <Ftfighter797@gail.com>
2023-08-29 16:26:21 +0700

    Add selectFIle Button and simplify the main code 	renamed:    src/templates/GUI.java -> src/GUI.java 	modified:   src/Main.java

--- src/GUI.java
+++ src/GUI.java
@@ -1,15 +1,26 @@
-package templates;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.HashSet;
 
 public class GUI extends JFrame {
+
     public void displayGUI() {
+        Sha256 sha256 = new Sha256();
+        MalwareDataset malwareDataset = new MalwareDataset();
+
+        HashSet<String> maliciousHashes = malwareDataset.getMaliciousHashes("data\\data.csv");
+
+
 
         // create secure icon
         ImageIcon secure = new ImageIcon("src\\templates\\secure.png");
         Image image = secure.getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH);
         secure = new ImageIcon(image);
+
+
 
         // create secure button
         JButton buttonSe = new JButton();
@@ -23,10 +34,14 @@ public class GUI extends JFrame {
         buttonSe.setVerticalTextPosition(JButton.BOTTOM);
         buttonSe.setForeground(Color.white);
 
+
+
         // create scan icon
         ImageIcon scan = new ImageIcon("src\\templates\\scan.png");
         Image image1 = scan.getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH);
         scan = new ImageIcon(image1);
+
+
 
         // create scan button
         JButton buttonSc = new JButton();
@@ -40,10 +55,14 @@ public class GUI extends JFrame {
         buttonSc.setVerticalTextPosition(JButton.BOTTOM);
         buttonSc.setForeground(Color.white);
 
+
+
         // create notification icon
         ImageIcon noti = new ImageIcon("src\\templates\\notification.png");
         Image image2 = noti.getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH);
         noti = new ImageIcon(image2);
+
+
 
         // create notification button
         JButton buttonNo = new JButton();
@@ -57,10 +76,15 @@ public class GUI extends JFrame {
         buttonNo.setVerticalTextPosition(JButton.BOTTOM);
         buttonNo.setForeground(Color.white);
 
+
+
         // create setting icon
         ImageIcon setting = new ImageIcon("src\\templates\\gear.png");
         Image image3 = setting.getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH);
         setting = new ImageIcon(image3);
+
+
+
 
         // create setting button
         JButton buttonSet = new JButton();
@@ -74,11 +98,16 @@ public class GUI extends JFrame {
         buttonSet.setVerticalTextPosition(JButton.BOTTOM);
         buttonSet.setForeground(Color.white);
 
+
+
+
         // create line label
         Icon line1 = new ImageIcon("src\\templates\\line.png");
         JLabel line = new JLabel();
         line.setBounds(10, 400, 80, 2);
         line.setIcon(line1);
+
+
 
         // create panel 1
         JPanel panel1 = new JPanel();
@@ -91,29 +120,74 @@ public class GUI extends JFrame {
         panel1.add(buttonSet);
         panel1.add(line);
 
+
+
         // create Antivirus icon
         ImageIcon antivi = new ImageIcon("src\\templates\\antivirus.png");
         Image image4 = antivi.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
         antivi = new ImageIcon(image4);
 
-        // create Click scan button
-        JButton buttonClickScan = new JButton();
-        buttonClickScan.setBounds(280, 200, 200, 200);
-        buttonClickScan.setOpaque(false);
-        buttonClickScan.setContentAreaFilled(false);
-        buttonClickScan.setBorderPainted(false);
-        buttonClickScan.setText("Scanning");
-        buttonClickScan.setIcon(antivi);
-        buttonClickScan.setHorizontalTextPosition(JButton.CENTER);
-        buttonClickScan.setVerticalTextPosition(JButton.BOTTOM);
-        buttonClickScan.setForeground(Color.white);
+
+
+
+        // Add button to select files
+        JButton selectFileButton = new JButton();
+        selectFileButton.setBounds(280, 200, 200, 200);
+        selectFileButton.setOpaque(false);
+        selectFileButton.setContentAreaFilled(false);
+        selectFileButton.setBorderPainted(false);
+        selectFileButton.setText("Scanning");
+        selectFileButton.setIcon(antivi);
+        selectFileButton.setHorizontalTextPosition(JButton.CENTER);
+        selectFileButton.setVerticalTextPosition(JButton.BOTTOM);
+        selectFileButton.setForeground(Color.white);
+
+
+
 
         // create panel 2
         JPanel panel2 = new JPanel();
         panel2.setBackground(new Color(36, 36, 36));
         panel2.setBounds(100, 0, 850, 650);
         panel2.setLayout(null);
-        panel2.add(buttonClickScan);
+        panel2.add(selectFileButton);
+
+        
+
+
+
+
+
+
+        // Add an ActionListener to the selectFileButton
+        selectFileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                int result = fileChooser.showOpenDialog(null);
+
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = fileChooser.getSelectedFile();
+                    String selectedFilePath = selectedFile.getAbsolutePath();
+
+                    String sha256Hash = sha256.getSHA256Hash(selectedFilePath);
+                    System.out.println("SHA-256 Hash of the file: " + sha256Hash);
+                    boolean isMalicious = maliciousHashes.contains(sha256Hash);
+
+                    if (isMalicious) {
+                        System.out.println("The file is detected as malicious.");
+                    } else {
+                        System.out.println("The file is not detected as malicious.");
+                    }
+                }
+            }
+        });
+
+
+
+
+
+
 
         // create main layout
         ImageIcon logo = new ImageIcon("src\\templates\\antivirus.png");
@@ -127,6 +201,5 @@ public class GUI extends JFrame {
         frame.setIconImage(logo.getImage());
         frame.add(panel1);
         frame.add(panel2);
-
     }
 }
